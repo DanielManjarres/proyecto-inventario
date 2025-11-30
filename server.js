33d06894276ea72c17c7f5c74e656063171177fe
Daniel Manjarres <danielmanjarres6@gmail.com>
2025-11-29 23:23:12 -0500

    prueba

--- server.js
+++ server.js
@@ -7,4 +7,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en http://localhost:${PORT}`);
+  console.log("DB URL desde Railway:", process.env.DATABASE_URL);
 });
--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
   dialect: "mysql",
   logging: false
 });
 
 export default sequelize;
+    
