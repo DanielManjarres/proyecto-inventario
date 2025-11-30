adf93ec42e52b36b1593982f94dcc479b29fc561
Daniel Manjarres <danielmanjarres6@gmail.com>
2025-11-30 00:01:54 -0500

    prueba1.4

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 import app from "./src/app.js";
-import "dotenv/config"; 
+import dotenv from "dotenv";
+import sequelize from "./src/config/database.js";
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-console.log("DATABASE_URL:", process.env.MYSQL_URL || process.env.DATABASE_URL);
+async function start() {
+  try {
+    console.log("DATABASE_URL:", process.env.DATABASE_URL);
+    
+    await sequelize.authenticate();
+    console.log("Conexión a DB exitosa.");
 
-app.listen(PORT, () => {
-  console.log(`Servidor iniciado en http://localhost:${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Servidor iniciado en http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error conectando a la DB:", error);
+  }
+}
+
+start();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
+import sequelize from "../config/database.js";
 
 const router = Router();
 
 router.get("/status", (req, res) => {
-    res.json({ message: "API Inventario funcionando" });
+  res.json({ message: "API Inventario funcionando" });
+});
+
+router.get("/db-test", async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    res.json({ status: "ok", message: "DB conectada" });
+  } catch (error) {
+    res.status(500).json({ status: "error", error: error.message });
+  }
 });
 
 export default router;
