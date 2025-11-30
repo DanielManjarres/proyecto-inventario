c4389c517223b30cabe0d0800e3ce3db90e81484
Daniel Manjarres <danielmanjarres6@gmail.com>
2025-11-29 22:52:15 -0500

    railway

--- src/config/database.js
+++ src/config/database.js
@@ -1,24 +1,10 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const dbConfig = process.env.DATABASE_URL
-  ? {
-      url: process.env.DATABASE_URL,
-      options: {
-        dialect: "mysql",
-        logging: false,
-      },
-    }
-  : {
-      url: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-      options: {
-        dialect: "mysql",
-        logging: false,
-      },
-    };
-
-const sequelize = new Sequelize(dbConfig.url, dbConfig.options);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+  logging: false
+});
 
 export default sequelize;
