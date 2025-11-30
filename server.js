4a85ea94eb78f2c8856c43efe5d95c4eb1a2abf7
Daniel Manjarres <danielmanjarres6@gmail.com>
2025-11-29 23:26:27 -0500

    prueba1.1

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import app from "./src/app.js";
-import dotenv from "dotenv";
+import 'dotenv/config';
 
-dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en http://localhost:${PORT}`);
   console.log("DB URL desde Railway:", process.env.DATABASE_URL);
+  console.log("VARIABLE FINAL:", process.env.DATABASE_URL);
+
 });
