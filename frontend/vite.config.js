577071750a68fd621ee056c10224d2ca27d03529
Daniel Manjarres <danielmanjarres6@gmail.com>
2025-12-02 01:14:22 -0500

    yaaaaaa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,13 +7,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  preview: {
-    host: true,  // Esto permite que escuche en todos los hosts
-    port: Number(process.env.PORT) || 3000,
-    allowedHosts: ['all'],  // ← Array con 'all' en lugar de string
-    // O más seguro (recomendado para Railway):
-    // allowedHosts: ['.up.railway.app', 'frontend-production-2351.up.railway.app']
-  },
+ preview: {
+  host: true,
+  port: Number(process.env.PORT) || 3000,
+  allowedHosts: ['frontend-production-2351.up.railway.app', '.up.railway.app'],
+},
 
   server: {
     host: true,
