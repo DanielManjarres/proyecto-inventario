4e78b4d7942557d26837f1268e705e8964de539d
Daniel Manjarres <danielmanjarres6@gmail.com>
2025-12-02 00:53:54 -0500

    frontend1.2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  preview: {
+    host: true,
+    port: 3000,
+    allowedHosts: 'all',           
+  },
+
+  server: {
+    host: true,
+    port: 3000,
+  },
 })
